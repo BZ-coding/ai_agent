545978715194b60115065f74abc0ee7baba1bbff
zhangsheng377 <435878393@qq.com>
2024-08-08 01:00:10 +0800

    改进了人工标注页面

--- generate_finetune_sample/04_manual_annotate_samples_gradio.py
+++ generate_finetune_sample/04_manual_annotate_samples_gradio.py
@@ -81,8 +81,11 @@ def delete_func():
 
 def convert_message_to_dataframe(messages):
     messages_ = []
+    id = 0
     for message in messages:  # After edit, the \n will miss
-        messages_.append({"role": message["role"], "content": message["content"].replace("\n", "\n\u0000")})
+        # messages_.append({"id": id, "role": message["role"], "content": message["content"].replace("\n", "\n\u0000")})
+        messages_.append({"id": id, "role": message["role"], "content": message["content"]})
+        id += 1
     return pd.DataFrame.from_records(messages_)
 
 
@@ -92,10 +95,10 @@ def convert_dataframe_to_current_message(dataframe):
     result = []
     for message in messages:
         content = message['content']
-        if '\n' not in content:
-            content = content.replace("\u0000", "\n")
-        else:
-            content = content.replace("\u0000", "")  # don't edit cell will have \u0000
+        # if '\n' not in content:
+        #     content = content.replace("\u0000", "\n")
+        # else:
+        #     content = content.replace("\u0000", "")  # don't edit cell will have \u0000
         result.append({"role": message["role"], "content": content})
     current_messages = result
 
@@ -105,17 +108,44 @@ def continue_conversation(dataframe):
     convert_dataframe_to_current_message(dataframe)
     print("continue_conversation : ", current_messages)
     query = current_data['query']
-    for response_messages in ai_agent.ai_agent_chat(query=query, temperature=0.0, is_print=True, messages=current_messages):
+    for response_messages in ai_agent.ai_agent_chat(query=query, temperature=0.0, is_print=True,
+                                                    messages=current_messages):
         current_messages = response_messages
         return convert_message_to_dataframe(current_messages)
 
 
+def select_conversation(index):
+    try:
+        id = int(index)
+    except:
+        return ""
+    if id not in range(len(current_messages)):
+        return ""
+    return current_messages[id]["content"]
+
+
+def change_conversation(index, content):
+    global current_messages
+    try:
+        id = int(index)
+    except:
+        return convert_message_to_dataframe(current_messages)
+    if id not in range(len(current_messages)):
+        return convert_message_to_dataframe(current_messages)
+    current_messages[id]["content"] = content
+    return convert_message_to_dataframe(current_messages)
+
+
 with gr.Blocks() as demo:
     gr.Markdown("# Manually Annotate Samples")
-    outputs = gr.DataFrame(headers=["role", "content"],
-                           datatype=["str", "markdown"],
-                           interactive=True,
-                           wrap=True)
+    with gr.Row():
+        with gr.Column():
+            edit_index_textbox = gr.Textbox(label="Edit index")
+            edit_content_textbox = gr.Textbox(label="Edit content", interactive=True)
+        outputs = gr.DataFrame(headers=["id", "role", "content"],
+                               datatype=["number", "str", "markdown"],
+                               interactive=False,
+                               wrap=True)
     with gr.Row():
         start_button = gr.Button("Start & Next")
         continue_button = gr.Button("Continue conversation")
@@ -125,6 +155,9 @@ with gr.Blocks() as demo:
     save_button.click(fn=save_func, inputs=[outputs], outputs=[outputs])
     del_button.click(fn=delete_func, outputs=[outputs])
     continue_button.click(fn=continue_conversation, inputs=[outputs], outputs=[outputs])
+    edit_index_textbox.input(fn=select_conversation, inputs=[edit_index_textbox], outputs=[edit_content_textbox])
+    edit_content_textbox.change(fn=change_conversation, inputs=[edit_index_textbox, edit_content_textbox],
+                                outputs=[outputs])
 
     # commit_btn.click(fn=add_text, inputs=[chatbot, txt], outputs=[chatbot, txt]).then(
     #     bot, chatbot, chatbot, api_name="bot_response"
@@ -134,5 +167,3 @@ demo.launch(server_name="0.0.0.0",
             share=False,
             debug=True,
             )
-
-# https://blog.csdn.net/jclian91/article/details/132417892
