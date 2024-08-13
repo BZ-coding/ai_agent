a717c5d208becb2249cf7173bd13b533384b8ae2
zhangsheng377 <435878393@qq.com>
2024-08-14 00:28:14 +0800

    尝试传递tools

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -16,7 +16,7 @@ class ChatBot:
             api_key='ollama',  # required but ignored
         )
 
-    def _run_conversation(self, messages: Union[List[Dict[str, str]], str], temperature, stream, stop):
+    def _run_conversation(self, messages: Union[List[Dict[str, str]], str], temperature, tools, stream, stop):
         if isinstance(messages, str):
             messages = [{"role": "user", "content": messages}]
         response = self.client.chat.completions.create(
@@ -25,25 +25,30 @@ class ChatBot:
             stream=stream,
             model=MODEL_NAME,
             stop=stop,
+            tools=tools,
         )
         return response
 
-    def _chat(self, messages: Union[List[Dict[str, str]], str], temperature, stop):
-        response = self._run_conversation(messages=messages, temperature=temperature, stream=False, stop=stop)
+    def _chat(self, messages: Union[List[Dict[str, str]], str], temperature, tools, stop):
+        response = self._run_conversation(messages=messages, temperature=temperature, stream=False, tools=tools,
+                                          stop=stop)
+        print(response)
         return response.choices[0].message.content
 
-    def _stream_chat(self, messages: Union[List[Dict[str, str]], str], temperature, stop):
-        response = self._run_conversation(messages=messages, temperature=temperature, stream=True, stop=stop)
+    def _stream_chat(self, messages: Union[List[Dict[str, str]], str], temperature, tools, stop):
+        response = self._run_conversation(messages=messages, temperature=temperature, stream=True, tools=tools,
+                                          stop=stop)
         for token in response:
             if token.choices[0].finish_reason is not None:
                 continue
             yield token.choices[0].delta.content
 
-    def chat(self, messages: Union[List[Dict[str, str]], str], temperature=0.6, stop=NOT_GIVEN, stream=False):
+    def chat(self, messages: Union[List[Dict[str, str]], str], temperature=0.6, tools=NOT_GIVEN, stop=NOT_GIVEN,
+             stream=False):
         if not stream:
-            return self._chat(messages=messages, temperature=temperature, stop=stop)
+            return self._chat(messages=messages, temperature=temperature, tools=tools, stop=stop)
         else:
-            return self._stream_chat(messages=messages, temperature=temperature, stop=stop)
+            return self._stream_chat(messages=messages, temperature=temperature, tools=tools, stop=stop)
 
 
 if __name__ == '__main__':
@@ -58,3 +63,27 @@ if __name__ == '__main__':
         print(token, end='', flush=True)
         sleep(0.1)
     print('\n')
+
+    tools = [{
+        'type': 'function',
+        'function': {
+            'name': 'web_search',
+            'description': 'Useful for when you need to answer questions about current events.',
+            'parameters': {
+                'type': 'object',
+                'properties': {
+                    'query': {
+                        'type': 'string',
+                        'description': 'Tsearch query of web',
+                    },
+                },
+                'required': ['query'],
+            },
+        },
+    },
+    ]
+    message = [{'role': 'user',
+                'content': '你是一个在线个人助手。当被问及你所不知道的问题时，可以调用工具从网上搜索相关信息以回答问题。问题:金陵中学所在的行政区有多少人?'}]
+    print(chatbot.chat(messages=message, tools=tools, stream=False))
+    print('\n')
+    # 传递tools的方法好像没有用。最好也是最清晰的方法就是我自己组装tools的prompt。
